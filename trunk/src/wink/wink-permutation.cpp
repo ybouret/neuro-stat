fd08d872200f6a4ba443d60bf0bac4393820625e
yann.bouret@gmail.com <yann.bouret@gmail.com@d870e702-db5d-e285-c176-38c699937b0f>
2012-07-04 08:43:10 +0000

    changed tests location

--- trunk/src/wink/wink-permutation.cpp
+++ trunk/src/wink/wink-permutation.cpp
@@ -11,13 +11,16 @@ namespace wink
     {
         assert( !(indices==NULL&&n>0) );
         for( size_t i=0; i < n; ++i ) indices[i] = i;
-        if(n>0)
+        if(n>1)
         {
             for( size_t i=n-1;i>0;--i)
             {
                 const size_t j = size_t( floor( (i*uniform_generator()) + 0.5 ));
                 assert(j<=i);
                 assert(j>=0);
+                const size_t tmp = indices[i];
+                indices[i] = indices[j];
+                indices[j] = tmp;
             }
         }
     }
