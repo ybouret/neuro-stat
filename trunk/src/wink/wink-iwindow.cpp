b998c33a87548ae0a829e58caf6de3dda218b40f
yann.bouret@gmail.com <yann.bouret@gmail.com@d870e702-db5d-e285-c176-38c699937b0f>
2012-07-04 09:37:27 +0000

    encoded coincidences with window

--- trunk/src/wink/wink-iwindow.cpp
+++ trunk/src/wink/wink-iwindow.cpp
@@ -4,6 +4,8 @@
 namespace wink
 {
     
+    iwindow:: iwindow() throw() : indexA(0), indexB(0), length(0) {}
+    iwindow:: ~iwindow() throw() {}
     
     void iwindow:: initialize( double a, double b, const double *X )
     {
@@ -14,6 +16,38 @@ namespace wink
         if( foundA && foundB && (indexA <= indexB) )
         {
             length = indexB - indexA + 1;
+        }
+    }
+    
+    
+    size_t coincidences(const double   *X, 
+                        const iwindow  *Wx,
+                        const double   *Y,
+                        const iwindow  *Wy,
+                        const double    delta)
+    {
+        assert(X!=NULL);
+        assert(Y!=NULL);
+        assert(Wx!=NULL);
+        assert(Wy!=NULL);
+        if( Wx->length>0 && Wy->length>0 )
+        {
+            const size_t Nx = size_t(X[0]);
+            assert( Wx->indexA>0 );
+            assert( Wx->indexB>0 );
+            assert( Wx->indexA <= Nx );
+            assert( Wx->indexB <= Nx );
+            
+            const size_t Ny = size_t(Y[0]);
+            assert( Wy->indexA>0 );
+            assert( Wy->indexB>0 );
+            assert( Wy->indexA <= Ny );
+            assert( Wy->indexB <= Ny );
+            return coincidences(X+Wx->indexA, Wx->length, Y+Wy->indexA, Wy->length, delta);
+        }
+        else 
+        {
+            return 0; //! no data in at least one window
         }
     }
 
--- trunk/src/wink/wink.hpp
+++ trunk/src/wink/wink.hpp
@@ -60,8 +60,11 @@ namespace wink
     bool find_index_before( double b, const double *X, size_t &i );
     
     //! location of data
-    struct iwindow
+    class iwindow
     {
+    public:
+        iwindow() throw();
+        ~iwindow() throw();
         size_t indexA; //!< first index
         size_t indexB; //!< last  index
         size_t length; //!< indexB - indexA  +1, 0 is no valid data
@@ -74,7 +77,25 @@ namespace wink
          between two consecutive X, so the window is empty.
          */
         void   initialize( double a, double b, const double *X );
+    private:
+        iwindow( const iwindow & );
+        iwindow & operator=( const iwindow & );
     };
+    
+    
+    //! detect coincidences
+    /**
+     \param X a record X[0] = Nx, X[1..Nx] are the ORDERED data
+     \param Wx a valid window associated to X
+     \param Y a record Y[0] = Ny, Y[1..Ny] are the ORDERED data
+     \param Wy a valid window associated to Y
+     \return the number of coincidences within lag delta
+     */
+    size_t coincidences(const double   *X, 
+                        const iwindow  *Wx,
+                        const double   *Y,
+                        const iwindow  *Wy,
+                        const double    delta);
     
     //! build a random permutation
     /**
@@ -86,6 +107,9 @@ namespace wink
     
     //! build an identity vector
     void build_identity( size_t *indices, size_t num );
+    
+    
+    
     
     
 }
