6aeddfdc02137822ea384cda55b306a6c3aa5a31
yann.bouret@gmail.com <yann.bouret@gmail.com@d870e702-db5d-e285-c176-38c699937b0f>
2012-07-04 09:23:26 +0000

    built uniform permutations

---
+++ src/wink/tests/permutation.cpp
@@ -0,0 +1,59 @@
+#include "../wink.hpp"
+#include <iostream>
+
+
+
+int main( int argc, char *argv[] )
+{
+    
+    try
+    {
+        static const size_t COUNT = 2048;
+        wink::init_alea();
+        FILE *fp = fopen("perm.dat","wb");
+        if( !fp )
+        {
+            std::cerr << "Can't open file" << std::endl;
+            throw;
+        }
+        
+        for( size_t iter = 1; iter <= 1024; ++iter )
+        {
+            const size_t   n       = 10 + size_t( wink::alea() * 10 );
+            size_t        *indices = new size_t[ n ];
+            double ave1 = 0;
+            double ave2 = 0;
+            double ave3 = 0;
+            const size_t idx1=0;
+            const size_t idx2=n/2;
+            const size_t idx3=n-1;
+            for(size_t count=1; count <= COUNT; ++count )
+            {
+                wink::build_permutation(indices,n);
+#if 0
+                for( size_t i=0; i < n; ++i )
+                    std::cerr << " " << indices[i];
+                std::cerr << std::endl;
+#endif
+                ave1 += indices[idx1];
+                ave2 += indices[idx2];
+                ave3 += indices[idx3];
+            }
+            ave1 /= COUNT;
+            ave1 /= n-1;
+            ave2 /= COUNT;
+            ave2 /= n-1;
+            ave3 /= COUNT;
+            ave3 /= n-1;
+            fprintf(fp,"%u %.4f %.4f %.4f\n", unsigned(iter), ave1, ave2, ave3 );
+            delete [] indices;
+        }
+        fclose(fp);
+        return 0;
+    }
+    catch(...)
+    {
+        std::cerr << "Error detected" << std::endl;
+    }
+    return 1;
+}
--- src/wink/wink-permutation.cpp
+++ src/wink/wink-permutation.cpp
@@ -5,19 +5,32 @@
 
 namespace wink
 {
-    
-        
-    void build_permutation( size_t *indices, size_t n, double (*uniform_generator)() )
+    void build_identity( size_t *indices, size_t n)
     {
         assert( !(indices==NULL&&n>0) );
         for( size_t i=0; i < n; ++i ) indices[i] = i;
+    }
+    
+    static inline
+    size_t __full( double (*uniform_generator)() )
+    {
+        size_t ans = 0;
+        for( size_t i=0; i < sizeof(size_t); ++i )
+        {
+            ans <<= 1;
+            ans |= (uniform_generator()>0.5?1:0);
+        }
+        return ans;
+    }
+    
+    void build_permutation( size_t *indices, size_t n, double (*uniform_generator)() )
+    {
+        build_identity(indices, n);
         if(n>1)
         {
             for( size_t i=n-1;i>0;--i)
             {
-                const size_t j = size_t( floor( (i*uniform_generator()) + 0.5 ));
-                assert(j<=i);
-                assert(j>=0);
+                const size_t j = __full(uniform_generator) % (i+1);
                 const size_t tmp = indices[i];
                 indices[i] = indices[j];
                 indices[j] = tmp;
