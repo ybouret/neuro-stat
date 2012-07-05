ed717592a782d368931c9690035f1f54ff98b4fb
yann.bouret@gmail.com <yann.bouret@gmail.com@d870e702-db5d-e285-c176-38c699937b0f>
2012-07-05 15:02:16 +0000

    changed to wink_ser.cpp

--- trunk/src/wink_R/build.sh
+++ trunk/src/wink_R/build.sh
@@ -1,2 +1,2 @@
-MAKEFLAGS='CXXFLAGS=-O2\ -Wall\ -DNDEBUG=1\ -pipe' R CMD SHLIB -c --preclean -o wink_ser.so ../wink/*.cpp wink.cpp -O2 
+MAKEFLAGS='CXXFLAGS=-O2\ -Wall\ -DNDEBUG=1\ -pipe' R CMD SHLIB -c --preclean -o wink_ser.so ../wink/*.cpp wink_ser.cpp -O2 
 
