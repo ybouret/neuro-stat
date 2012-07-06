b00fd73818e77a073e7d96f59181ee945b17cc74
yann.bouret@gmail.com <yann.bouret@gmail.com@d870e702-db5d-e285-c176-38c699937b0f>
2012-07-06 10:24:56 +0000

    added raw makefile with extension dependencies

--- trunk/src/wink_R/build.sh
+++
@@ -1,4 +0,0 @@
-MAKEFLAGS='CXXFLAGS=-O2\ -Wall\ -DNDEBUG=1\ -pipe' R CMD SHLIB -c --preclean -o wink_ser.so ../wink/*.cpp wink_ser.cpp 
-MAKEFLAGS='CXXFLAGS=-O2\ -Wall\ -DNDEBUG=1\ -pipe' R CMD SHLIB -c --preclean -o wink_par.so ../wink/*.cpp ../wink/par/*.cpp wink_par.cpp 
-
-
