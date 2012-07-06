9e0dde68e3af1a12841bb0b2356cead748211595
yann.bouret@gmail.com <yann.bouret@gmail.com@d870e702-db5d-e285-c176-38c699937b0f>
2012-07-06 12:36:09 +0000

    wink loader

---
+++ trunk/src/wink_R/load_wink.R
@@ -0,0 +1,68 @@
+########################################################################
+##
+## loading DLL
+##
+########################################################################
+
+## build the DLL names
+wink_ser_dll <- paste("wink_ser", .Platform$dynlib.ext, sep="")
+wink_par_dll <- paste("wink_par", .Platform$dynlib.ext, sep="")
+print(c(paste("serial: ",wink_ser_dll,sep=""),paste("parallel: ",wink_par_dll,sep="")))
+
+## load them
+dyn.load(wink_ser_dll)
+dyn.load(wink_par_dll)
+
+########################################################################
+##
+## check arguments
+##
+########################################################################
+wink_bootstrap_check_args <- function(N1,N2,intervals,delta,B)
+{
+	## check arguments in R
+	if (!is.matrix(N1))
+		stop("N1 must be a matrix")
+		
+	if (!is.matrix(N2))
+		stop("N2 must be a matrix")
+		
+	if( !is.matrix(invervals) )
+		stop("intervals must be a matrix")
+		
+	if( !is.real(delta) )
+		stop("delta must be a real")
+		
+	if( !is.real(B) )
+		stop("B must be a real/integer")
+}
+
+########################################################################
+##
+## Define the sequential function
+##
+########################################################################
+bootstrap_pvalues_ser <- function(N1,N2,intervals,delta,B)
+{
+	# common arguments check
+	wink_bootstrap_check_args(N1,N2,intervals,delta,B);
+		
+	#return: the pvalues associated to each interval
+	.Call("wink_ser", N1, N2,intervals,delta,B );
+	
+}
+
+
+bootstrap_pvalues_par <- function(N1,N2,intervals,delta,B,num_threads)
+{
+	
+	# common arguments check
+	wink_bootstrap_check_args(N1,N2,intervals,delta,B);
+	
+	# num threads check
+	if( !is.real(num_threads) )
+		stop("num_threads must be a real/integer")
+		
+	#return: the pvalues associated to the windows
+	.Call("wink_par", N1, N2,windows,delta,B,num_threads);
+}
