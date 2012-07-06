64a5372571bdbdee58538ca4a03e69707dcf3b41
yann.bouret@gmail.com <yann.bouret@gmail.com@d870e702-db5d-e285-c176-38c699937b0f>
2012-07-06 12:36:09 +0000

    wink loader

--- src/wink_R/wink.R
+++ src/wink_R/wink.R
@@ -1,11 +1,9 @@
 
-wink_ser_dll <- paste("wink_ser", .Platform$dynlib.ext, sep="")
-wink_par_dll <- paste("wink_par", .Platform$dynlib.ext, sep="")
 
-dyn.load(wink_ser_dll)
-dyn.load(wink_par_dll);
 
+source("load_wink.R");
 source("simulationPP.R")
+
 neurons <- simulPP(0,2,50,40);
 
 #windows <- matrix( data=0, nrow = 2, ncol=3 );
@@ -13,60 +11,18 @@ neurons <- simulPP(0,2,50,40);
 #windows[1,2]=0.1; windows[2,2]=0.2;
 #windows[1,3]=0.2; windows[2,3]=0.3;
 
-a=seq(0,1.9,by=0.005)
+a=seq(0,1.9,by=0.01)
 b=a+0.1
-windows=t(matrix(c(a,b),ncol=2))
+intervals=t(matrix(c(a,b),ncol=2))
 
-bootstrap_pvalue_ser <- function(N1,N2,windows,delta,B)
-{
-	## check arguments in R
-	if (!is.matrix(N1))
-		stop("N1 must be a matrix")
-	if (!is.matrix(N2))
-		stop("N2 must be a matrix")
-	if( !is.matrix(windows) )
-		stop("windows must be a matrix")
-	if( !is.real(delta) )
-		stop("delta must be a real")
-		
-	if( !is.real(B) )
-		stop("B must be a real/integer")
-	
-	
-	.Call("wink_ser", N1, N2,windows,delta,B );
-	#return: the pvalues associated to the windows
-}
-
-
-bootstrap_pvalue_par <- function(N1,N2,windows,delta,B,num_threads)
-{
-	## check arguments in R
-	if (!is.matrix(N1))
-		stop("N1 must be a matrix")
-	if (!is.matrix(N2))
-		stop("N2 must be a matrix")
-	if( !is.matrix(windows) )
-		stop("windows must be a matrix")
-	if( !is.real(delta) )
-		stop("delta must be a real")
-		
-	if( !is.real(B) )
-		stop("B must be a real/integer")
-	
-	if( !is.real(num_threads) )
-		stop("num_threads must be a real/integer")
-		
-	.Call("wink_par", N1, N2,windows,delta,B,num_threads);
-	#return: the pvalues associated to the windows
-}
 
 stamp = proc.time();
-bootstrap_pvalue_ser(neurons[[1]],neurons[[2]],windows,0.01,10000);
+bootstrap_pvalues_ser(neurons[[1]],neurons[[2]],intervals,0.01,10000);
 ser_tmx = proc.time() - stamp;
 ser_tmx[3];
 
 stamp = proc.time();
-bootstrap_pvalue_par(neurons[[1]],neurons[[2]],windows,0.01,10000,4);
+bootstrap_pvalues_par(neurons[[1]],neurons[[2]],intervals,0.01,10000,4);
 par_tmx = proc.time() - stamp;
 par_tmx[3];
 
