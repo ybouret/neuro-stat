b2204252f7e915dcb0df93820e7821691d65d1f4
yann.bouret@gmail.com <yann.bouret@gmail.com@d870e702-db5d-e285-c176-38c699937b0f>
2013-01-14 14:18:45 +0000

    wink:experiment

--- trunk/v2/src/wink/record.cpp
+++ trunk/v2/src/wink/record.cpp
@@ -1,3 +1,24 @@
 #include "./record.hpp"
+#include "../pyck/exception.hpp"
 
-
+namespace wink
+{
+       
+    record::record( const double *data ) throw():
+    addr( data    )
+    {
+    }
+    
+    size_t record:: size() const throw()
+    {
+        return size_t(addr[0]);
+    }
+    
+    double record:: operator[]( size_t indx ) const throw()
+    {
+        assert( indx > 0 );
+        assert( indx <= size() );
+        return addr[indx];
+    }
+    
+}
--- trunk/v2/src/wink/record.hpp
+++ trunk/v2/src/wink/record.hpp
@@ -1,12 +1,30 @@
 #ifndef WINK_RECORD_INCLUDED
 #define WINK_RECORD_INCLUDED 1
 
+#include "../pyck/os.hpp"
+
 namespace wink
 {
+    //! one neuronal record, R kind
 	class record
-{
-};
-
+    {
+    public:
+        //! build from data
+        /**
+         \param   data data[0] = n, data[1..n]=tops
+         \warning data must be available, no memory is allocated !
+         */
+        explicit record( const double *data ) throw();
+        size_t size() const throw();
+        
+        inline double operator[]( size_t indx ) const throw();
+        
+    private:
+        const double *addr; //!< X[1..n]
+        record( const record & );
+        record&operator=(const record&);
+    };
+    
 }
 
 #endif
