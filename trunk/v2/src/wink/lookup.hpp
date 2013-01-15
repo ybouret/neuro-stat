c6c20b6f5738e9046d02abf3aaa37360faf60c8f
yann.bouret@gmail.com <yann.bouret@gmail.com@d870e702-db5d-e285-c176-38c699937b0f>
2013-01-15 09:38:35 +0000

    v2: added lookup

---
+++ trunk/v2/src/wink/lookup.hpp
@@ -0,0 +1,39 @@
+/**
+ \file 
+ \brief Lookup indices for windows construction
+ */
+
+#ifndef WINK_LOOKUP_INCLUDED
+#define WINK_LOOKUP_INCLUDED 1
+
+#include <cstddef>
+
+namespace wink
+{
+    
+    //! Algorithms to find the proper indices
+    struct lookup
+    {
+        //! find index such that  X[i-1] < a <= X[i]
+        /**
+         \param a any real
+         \param X a record X[0] = Nx, X[1..Nx] are the ORDERED data
+         \param i the index
+         \return true if this is possible and i is the answer, false otherwise and i=0
+         */
+        static bool index_after( double a, const double *X, size_t &i );
+        
+        //! find index such that  X[i] <= b < X[i+1]
+        /**
+         \param b any real
+         \param X a record X[0] = Nx, X[1..Nx] are the ORDERED data
+         \param i the index
+         \return true if this is possible and i is the answer, false otherwise and i=0
+         */
+        static bool index_before( double b, const double *X, size_t &i );
+    };
+    
+}
+
+
+#endif
