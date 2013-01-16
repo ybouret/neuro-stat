023188368b3bb7f52b95784fcadafbc83dcf502b
yann.bouret@gmail.com <yann.bouret@gmail.com@d870e702-db5d-e285-c176-38c699937b0f>
2013-01-16 09:41:36 +0000

    v2: rethink drawings

--- trunk/v2/src/wink/drawing.cpp
+++ trunk/v2/src/wink/drawing.cpp
@@ -3,27 +3,4 @@
 namespace wink
 {
     
-    drawing:: ~drawing() throw()
-    {
-        delete []reg;;
-    }
-    
-    drawing:: drawing( size_t n ) :
-    trials( n ),
-    pairs( trials * trials ),
-    reg( new ipair[pairs] )
-    {
-        ipair *p = reg;
-        for( size_t i=0; i < trials; ++i )
-        {
-            for(size_t j=0; j < trials; ++j )
-            {
-                p->first  = i;
-                p->second = j;
-                ++p;
-            }
-        }
-    }
-    
-    
 }
--- trunk/v2/src/wink/drawing.hpp
+++ trunk/v2/src/wink/drawing.hpp
@@ -6,37 +6,10 @@
 
 namespace wink
 {
-    typedef Pair<size_t,size_t> ipair;
+ 
     
-    class couple
-    {
-    public:
-        size_t first;
-        size_t second;
-        couple *next;
-        couple *prev;
-        
-    private:
-        couple(const couple &);
-        couple&operator=(const couple &);
-    };
     
-    class drawing
-    {
-    public:
-        explicit drawing( size_t n );
-        virtual ~drawing() throw();
-        
-        const size_t trials;
-        const size_t pairs;
-        
-    private:
-        drawing(const drawing & );
-        drawing&operator=(const drawing &);
-        
-        ipair *reg;
-        
-    };
+    
 }
 
 #endif
