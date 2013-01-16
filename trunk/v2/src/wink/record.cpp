5dae026965bdf0de1e7a95f55b1b1c54e67969bf
yann.bouret@gmail.com <yann.bouret@gmail.com@d870e702-db5d-e285-c176-38c699937b0f>
2013-01-16 10:14:01 +0000

    v2: tested with valgrind

--- trunk/v2/src/wink/record.cpp
+++ trunk/v2/src/wink/record.cpp
@@ -3,34 +3,35 @@
 
 namespace wink
 {
-    
+
     record::record( double *data ) throw():
     size( size_t(data[0]) ),
+    iwin(),
     addr( data    )
     {
     }
-    
-    
-    
+
+
+
     const double &record:: operator[]( size_t indx ) const throw()
     {
         assert( indx > 0 );
         assert( indx <= size );
         return addr[indx];
     }
-    
+
     double & record:: operator[]( size_t indx )  throw()
     {
         assert( indx > 0 );
         assert( indx <= size );
         return addr[indx];
     }
-    
+
     void record:: prepare( double a, double b ) throw()
     {
         iwin.initialize(a, b, addr);
     }
-    
+
     size_t record:: coincidences_with( const record &other, const double delta ) const throw()
     {
         return count_coincidences(addr,
@@ -39,6 +40,6 @@ namespace wink
                                   other.iwin,
                                   delta);
     }
-    
-    
+
+
 }
