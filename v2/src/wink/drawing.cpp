0bb4d7d696cf759494f989f5ef20cab7e5c95850
yann.bouret@gmail.com <yann.bouret@gmail.com@d870e702-db5d-e285-c176-38c699937b0f>
2013-01-16 11:05:07 +0000

    v2:typo update

--- v2/src/wink/drawing.cpp
+++ v2/src/wink/drawing.cpp
@@ -5,91 +5,91 @@
 
 namespace wink
 {
-
-void couple::kill( couple *c ) throw()
-{
-    assert(c);
-    delete c;
-}
-
-drawing:: ~drawing() throw()
-{
-    couple::list &self = *this;
-    pool.delete_with( couple::kill );
-    self.delete_with( couple::kill );
-}
-
-drawing:: drawing() throw() :
+    
+    void couple::kill( couple *c ) throw()
+    {
+        assert(c);
+        delete c;
+    }
+    
+    drawing:: ~drawing() throw()
+    {
+        couple::list &self = *this;
+        pool.delete_with( couple::kill );
+        self.delete_with( couple::kill );
+    }
+    
+    drawing:: drawing() throw() :
     couple::list(),
     pool()
-{
-
-}
-
-void drawing:: clear() throw()
-{
-    while( size ) pool.store( pop_back() );
-    assert( 0 == size );
-}
-
-
-couple *drawing:: query()
-{
-    couple *cpl = pool.size ? pool.query() : new couple;
-    memset(cpl,0,sizeof(couple));
-    return cpl;
-}
-
-void drawing:: reserve( size_t n )
-{
-    for(size_t i=0; i<n; ++i)
     {
-        couple *cpl = new couple;
+        
+    }
+    
+    void drawing:: clear() throw()
+    {
+        while( size ) pool.store( pop_back() );
+        assert( 0 == size );
+    }
+    
+    
+    couple *drawing:: query()
+    {
+        couple *cpl = pool.size ? pool.query() : new couple;
         memset(cpl,0,sizeof(couple));
-        pool.store( cpl );
+        return cpl;
     }
-}
-
-void drawing:: identity( size_t n )
-{
-    clear();
-    for( size_t i=0; i < n; ++i )
+    
+    void drawing:: reserve( size_t n )
     {
-        couple *cpl = query();
-        cpl->first = cpl->second = i;
-        push_back(cpl);
+        for(size_t i=0; i<n; ++i)
+        {
+            couple *cpl = new couple;
+            memset(cpl,0,sizeof(couple));
+            pool.store( cpl );
+        }
     }
-    assert(n==size);
-}
-
-void drawing:: permutation( size_t n, UniformGenerator &ran )
-{
-    clear();
-    if(n>0)
+    
+    void drawing:: identity( size_t n )
     {
-        C_Array<double> arr(n);
-        for(size_t i=0; i <n; ++i) arr[i] = i;
-        ran.shuffle(&arr[0],n);
+        clear();
         for( size_t i=0; i < n; ++i )
         {
             couple *cpl = query();
-            cpl->first  = i;
-            cpl->second = arr[i];
+            cpl->first = cpl->second = i;
             push_back(cpl);
         }
+        assert(n==size);
     }
-
-}
-
-std::ostream & operator<<( std::ostream &os, const drawing &d)
-{
-    os << "{";
-    for( const couple *c = d.head; c; c=c->next )
+    
+    void drawing:: permutation( size_t n, UniformGenerator &ran )
     {
-        os << " (" << c->first << "," << c->second << ")";
+        clear();
+        if(n>0)
+        {
+            C_Array<double> arr(n);
+            for(size_t i=0; i <n; ++i) arr[i] = i;
+            ran.shuffle(&arr[0],n);
+            for( size_t i=0; i < n; ++i )
+            {
+                couple *cpl = query();
+                cpl->first  = i;
+                cpl->second = arr[i];
+                push_back(cpl);
+            }
+        }
+        
     }
-    os << " }";
-    return os;
-}
-
+    
+    std::ostream & operator<<( std::ostream &os, const drawing &d)
+    {
+        os << "{";
+        for( const couple *c = d.head; c; c=c->next )
+        {
+            os << " (" << c->first << "," << c->second << ")";
+        }
+        os << " }";
+        return os;
+    }
+    
 }
