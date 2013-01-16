8306ec9ffe2b00ddbbf5b3f34941ebe7c8f75c12
yann.bouret@gmail.com <yann.bouret@gmail.com@d870e702-db5d-e285-c176-38c699937b0f>
2013-01-16 13:40:37 +0000

    v2: neurons and drawings upgrade

--- v2/src/wink/neurons.cpp
+++ v2/src/wink/neurons.cpp
@@ -17,6 +17,7 @@ namespace wink
     
     
     neurons::neurons() throw() :
+    drawing(),
     ran()
     {
         PYCK_LOCK(access);
@@ -24,5 +25,21 @@ namespace wink
         std::cerr << "s32=" << s32 << std::endl;
         ran.seed(s32);
     }
+ 
+   
+    size_t neurons:: total_coincidences( const neuron &N1, const neuron &N2, double delta ) const
+    {
+        size_t       count  = 0;
+        for( const couple *c = head;c;c=c->next)
+        {
+            assert(c->first  < N1.trials);
+            assert(c->second < N2.trials);
+            const record &r1 = N1[ c->first  ];
+            const record &r2 = N2[ c->second ];
+            count += r1.coincidences_with(r2, delta);
+        }
+        return count;
+    }
     
+
 }
