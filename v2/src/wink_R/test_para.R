39c20213872c75f000e4598beb0246c675197fe4
yann.bouret@gmail.com <yann.bouret@gmail.com@d870e702-db5d-e285-c176-38c699937b0f>
2013-01-18 16:26:58 +0000

    v2: ok for bootstrap_T

--- v2/src/wink_R/test_para.R
+++ v2/src/wink_R/test_para.R
@@ -1,20 +1,25 @@
 source("test_init.R")
 
-par(mfrow=c(2,1))
 
+par(mfrow=c(2,1))
 
 
 ## how to generate bootstrap with permutation, parallel code
 stamp=proc.time()
-alpha_perm_par <- wink_bootstrap_par(neurons[[1]],neurons[[2]],intervals,delta,B,"perm",2);
+alpha_perm_par <- wink_bootstrap_par(neurons[[1]],neurons[[2]],intervals,delta,B,"perm",4);
 perm_tmx_par=proc.time() - stamp;
 print(perm_tmx_par[3]);
+plot(intervals[1,],alpha_perm_par[1,],type="o",col="blue")
+points(intervals[1,],alpha_perm_par[2,],type="o",col="red")
+
 
 ## how to generate bootstrap with permutation
 stamp=proc.time()
 alpha_perm <- wink_bootstrap(neurons[[1]],neurons[[2]],intervals,delta,B,"perm");
 perm_tmx=proc.time() - stamp;
 print(perm_tmx[3]);
+plot(intervals[1,],alpha_perm_par[1,],type="o",col="blue")
+points(intervals[1,],alpha_perm_par[2,],type="o",col="red")
 
 alpha_diff = alpha_perm - alpha_perm_par;
 rms = norm(alpha_diff,"f")/prod(dim(alpha_diff));
