54c9be1b27630d5123192541483c85e5095501c8
yann.bouret@gmail.com <yann.bouret@gmail.com@d870e702-db5d-e285-c176-38c699937b0f>
2013-01-24 08:47:15 +0000

    implemented wink_single_T

--- trunk/v2/src/wink_R/test_single.R
+++ trunk/v2/src/wink_R/test_single.R
@@ -5,3 +5,8 @@ L=wink_single_H(neurons[[1]],neurons[[2]],0.1,0.1+delta,delta,B);
 
 count_plus =sum(L$Hc>=L$H);
 count_minus=sum(L$Hc<=L$H);
+
+L2=wink_single_T(neurons[[1]],neurons[[2]],0.1,0.1+delta,delta,B);
+
+count_plus2  = sum(L2$Tp>=L2$T);
+count_minus2 = sum(L2$Tp<=L2$T);
