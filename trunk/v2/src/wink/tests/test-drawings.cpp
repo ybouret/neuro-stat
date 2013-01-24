23a3e9d83d3f0e0a816742537b58d963543a16c0
yann.bouret@gmail.com <yann.bouret@gmail.com@d870e702-db5d-e285-c176-38c699937b0f>
2013-01-24 12:50:38 +0000

    upgraded drawings test

--- trunk/v2/src/wink/tests/test-drawings.cpp
+++ trunk/v2/src/wink/tests/test-drawings.cpp
@@ -1,10 +1,48 @@
 #include "../../pyck/rand32.hpp"
 #include "../../pyck/wtime.hpp"
 #include "../../wink/drawing.hpp"
+#include "../../pyck/c_array.hpp"
+#include "../../pyck/stats.hpp"
 
 #include <iostream>
 
 using namespace wink;
+
+
+
+static inline
+void display_stats( const C_Array<double> &arr, const size_t n )
+{
+    double ave=0, sig=0;
+    Stats::AverageAndStdDev(ave, sig, arr);
+    ave /= n-1;
+    sig /= n-1;
+    std::cerr << "Average=" << ave << ", StdDev=" << sig << std::endl;
+}
+
+static inline
+void perform( drawing &drw, DefaultUniformGenerator &ran)
+{
+    const size_t    n = 100;
+    const size_t    N = 4000;
+    C_Array<double> a_head(N);
+    C_Array<double> a_tail(N);
+    C_Array<double> a_mid(N);
+    for( size_t i=0; i < N; ++i )
+    {
+        drw.permutation(n, ran);
+        a_head[i] = drw.head->second; assert(drw.head->first==0);
+        a_tail[i] = drw.tail->second; assert(drw.tail->first==n-1);
+        a_mid[i]  = drw.fetch(n/2)->second;
+    }
+    std::cerr << "Head: " << std::endl;
+    display_stats(a_head,n);
+    std::cerr << "Tail: " << std::endl;
+    display_stats(a_tail,n);
+    std::cerr << "Middle: " << std::endl;
+    display_stats(a_mid,n);
+    
+}
 
 int main(int argc, char *argv[] )
 {
@@ -29,6 +67,11 @@ int main(int argc, char *argv[] )
             drw.replacement(10,ran);
             std::cerr << "drw=" << drw << std::endl;
         }
+        std::cerr << std::endl;
+
+        
+        perform( drw, ran );
+        
         return 0;
     }
     catch( const std::exception &e )
