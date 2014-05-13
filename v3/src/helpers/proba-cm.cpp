a02f1a02928af047b92dadb0f8c0dd558e5c7941
yann.bouret@gmail.com <yann.bouret@gmail.com@d870e702-db5d-e285-c176-38c699937b0f>
2014-05-13 07:46:02 +0000

    cleanup

--- v3/src/helpers/proba-cm.cpp
+++ v3/src/helpers/proba-cm.cpp
@@ -57,11 +57,11 @@ public:
     explicit Context() :
     A(),
     M(0),
-    lam(),
-    num(),
-    nmax(0),
+    lambda(),
+    occurs(),
+    nu(0),
     ratio(),
-    idx(),
+    kappa(),
     lmin(0),
     lmax(0),
     cmax(0),
@@ -70,17 +70,17 @@ public:
     
     virtual ~Context() throw() {}
     
-    imatrix_t A;      //!< matrix of coincidences
-    size_t    M;      //!< matrix size
-    ivector_t lam;    //!< all the different #coincidences
-    ivector_t num;    //!< #occurences of lambda
-    size_t    nmax;   //!< lam/num size
-    qvector_t ratio;  //!< single proba
-    ivector_t idx;    //!< indices to build proba
-    count_t   lmin;
-    count_t   lmax;
-    count_t   cmax;
-    qvector_t Proba;
+    imatrix_t      A;         //!< matrix of coincidences
+    size_t         M;         //!< matrix size
+    ivector_t      lambda;    //!< all the different #coincidences
+    ivector_t      occurs;    //!< #occurences of lambda
+    size_t         nu;        //!< lambda and occurs size: number of classes
+    qvector_t      ratio;     //!< single proba
+    vector<size_t> kappa;     //!< indices to build proba
+    count_t        lmin;
+    count_t        lmax;
+    count_t        cmax;
+    qvector_t      Proba;
     
     bool get_next( ios::istream &fp )
     {
@@ -104,10 +104,10 @@ private:
     inline void build_classes()
     {
         M = A.rows;
-        lam.free();
-        num.free();
+        lambda.free();
+        occurs.free();
         ratio.free();
-        idx.free();
+        kappa.free();
         imap_t dict;
         
         for(size_t i=1;i<=M;++i)
@@ -131,43 +131,44 @@ private:
                 }
             }
         }
-        nmax = dict.size();
-        lam.ensure(nmax);
-        num.ensure(nmax);
-        ratio.ensure(nmax);
-        idx.ensure(nmax);
+        nu = dict.size();
+        lambda.ensure(nu);
+        occurs.ensure(nu);
+        ratio.ensure(nu);
+        kappa.ensure(nu);
         
         for( imap_t::iterator i = dict.begin();i!=dict.end();++i)
         {
             const count_t k = i->key;
-            lam.push_back(k);
-            num.push_back(*i);
+            lambda.push_back(k);
+            occurs.push_back(*i);
         }
-        co_qsort(lam, num);
+        co_qsort(lambda, occurs);
         const size_t __den = M * (M-1);
-        for(size_t i=1;i<=nmax;++i)
+        for(size_t i=1;i<=nu;++i)
         {
-            const mpq p(num[i],__den);
+            const mpq p(occurs[i],__den);
             ratio.push_back(p);
         }
         
         mpq sum;
-        for(size_t i=1;i<=nmax;++i)
+        for(size_t i=1;i<=nu;++i)
         {
             sum += ratio[i];
         }
+        
         if(sum.den != 1 || sum.num != 1)
         {
             throw exception("unexpected sum of ratio failure");
         }
         
-        std::cerr << "nmax = " << nmax  << std::endl;
-        std::cerr << "lam  = " << lam   << std::endl;
-        std::cerr << "num  = " << num   << std::endl;
-        std::cerr << "ratio= " << ratio << std::endl;
+        std::cerr << "nu      = " << nu     << std::endl;
+        std::cerr << "lambda  = " << lambda << std::endl;
+        std::cerr << "occurs  = " << occurs << std::endl;
+        std::cerr << "ratio   = " << ratio  << std::endl;
 
-        lmin = lam[1];
-        lmax = lam[nmax];
+        lmin = lambda[1];
+        lmax = lambda[nu];
         cmax = lmax * M;
         std::cerr << "lambda_min=" << lmin << std::endl;
         std::cerr << "lambda_max=" << lmax << std::endl;
@@ -180,7 +181,10 @@ private:
     //
     // combinatorics...
     //__________________________________________________________________________
-
+    inline void compute_proba()
+    {
+        kappa.make(nu,0);
+    }
     
     
 };
