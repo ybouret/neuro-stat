08752b1722baeedf7e74a1b9e20824f4f581cc65
yann.bouret@gmail.com <yann.bouret@gmail.com@d870e702-db5d-e285-c176-38c699937b0f>
2014-05-13 06:19:13 +0000

    ratios are computed and checked

--- v3/src/helpers/proba-cm.cpp
+++ v3/src/helpers/proba-cm.cpp
@@ -50,50 +50,6 @@ bool load_matrix( imatrix_t &M, ios::istream &fp )
     return true;
 }
 
-static inline
-void build_classes(isorted_t       &lam,
-                   ivector_t       &num,
-                   const imatrix_t &M )
-{
-    lam.free();
-    num.free();
-    imap_t dict;
-    
-    for(size_t i=1;i<=M.rows;++i)
-    {
-        for(size_t j=1;j<=M.rows;++j)
-        {
-            if(i!=j)
-            {
-                const count_t k = M[i][j];
-                count_t *pNum = dict.search(k);
-                if(!pNum)
-                {
-                    if(!dict.insert(k,1))
-                        throw exception("unexpected insert failure");
-                }
-                else
-                {
-                    assert(*pNum>0);
-                    ++(*pNum);
-                }
-            }
-        }
-    }
-    const size_t nmax = dict.size();
-    lam.ensure(nmax);
-    num.ensure(nmax);
-    for( imap_t::iterator i = dict.begin();i!=dict.end();++i)
-    {
-        const count_t k = i->key;
-        if(!lam.insert(k))
-            throw exception("unexpected insert failure level-2");
-        num.push_back(*i);
-    }
-    std::cerr << "lam=" << lam << std::endl;
-    std::cerr << "num=" << num << std::endl;
-    
-}
 
 class Context
 {
@@ -102,34 +58,19 @@ public:
     explicit Context() {}
     virtual ~Context() throw() {}
     
-    imatrix_t A;    //!< matrix of coincidences
-    size_t    M;    //!< matrix size
-    isorted_t lam;  //!< all the different #coincidences
-    ivector_t num;   //!< #occurences of lambda
-    size_t    nmax;  //!< lam/num size
-    qvector_t prob;  //!< single proba
+    imatrix_t A;      //!< matrix of coincidences
+    size_t    M;      //!< matrix size
+    isorted_t lam;    //!< all the different #coincidences
+    ivector_t num;    //!< #occurences of lambda
+    size_t    nmax;   //!< lam/num size
+    qvector_t ratio;  //!< single proba
     
     bool get_next( ios::istream &fp )
     {
         if( load_matrix(A,fp) )
         {
-            M = A.rows;
-            build_classes(lam, num, A);
-            nmax = lam.size();
-            prob.free();
-            prob.ensure(nmax);
-            const size_t __den = M * (M-1);
-            for(size_t i=1;i<=nmax;++i)
-            {
-                const mpq p(num[i],__den);
-                prob.push_back(p);
-            }
-            std::cerr << "prob=" << prob << std::endl;
-            mpq sum;
-            for(size_t i=1;i<=nmax;++i)
-            {
-                sum += prob[i];
-            }
+            build_classes();
+            compute_ratios();
             return true;
         }
         else
@@ -138,6 +79,69 @@ public:
     }
     
 private:
+    inline void build_classes()
+    {
+        M = A.rows;
+        lam.free();
+        num.free();
+        imap_t dict;
+        
+        for(size_t i=1;i<=M;++i)
+        {
+            for(size_t j=1;j<=M;++j)
+            {
+                if(i!=j)
+                {
+                    const count_t k = A[i][j];
+                    count_t *pNum = dict.search(k);
+                    if(!pNum)
+                    {
+                        if(!dict.insert(k,1))
+                            throw exception("unexpected insert failure");
+                    }
+                    else
+                    {
+                        assert(*pNum>0);
+                        ++(*pNum);
+                    }
+                }
+            }
+        }
+        nmax = dict.size();
+        lam.ensure(nmax);
+        num.ensure(nmax);
+        for( imap_t::iterator i = dict.begin();i!=dict.end();++i)
+        {
+            const count_t k = i->key;
+            if(!lam.insert(k))
+                throw exception("unexpected insert failure level-2");
+            num.push_back(*i);
+        }
+        std::cerr << "lam=" << lam << std::endl;
+        std::cerr << "num=" << num << std::endl;
+    }
+    
+    inline void compute_ratios()
+    {
+        ratio.free();
+        ratio.ensure(nmax);
+        const size_t __den = M * (M-1);
+        for(size_t i=1;i<=nmax;++i)
+        {
+            const mpq p(num[i],__den);
+            ratio.push_back(p);
+        }
+        std::cerr << "ratio=" << ratio << std::endl;
+        mpq sum;
+        for(size_t i=1;i<=nmax;++i)
+        {
+            sum += ratio[i];
+        }
+        if(sum.den != 1 || sum.num != 1)
+        {
+            throw exception("unexpected sum of ratio failure");
+        }
+    }
     
 };
 
