6829510b5c57766c4ef7449ff399f5623535dd3f
yann.bouret@gmail.com <yann.bouret@gmail.com@d870e702-db5d-e285-c176-38c699937b0f>
2014-05-15 13:51:06 +0000

    WIP xts

---
+++ trunk/v3/src/wink_R/wink_xts.cpp
@@ -0,0 +1,88 @@
+#include "yocto/associative/map.hpp"
+#include "yocto/mpa/rational.hpp"
+#include "yocto/sequence/vector.hpp"
+#include "yocto/R/R++.hpp"
+
+using namespace yocto;
+
+extern "C"
+SEXP wink_xts( SEXP RCM ) throw()
+{
+    try
+    {
+        //______________________________________________________________________
+        //
+        // We have a R matrix=>convert it
+        //______________________________________________________________________
+        RMatrix<double> CM( RCM );
+        if(CM.cols!=CM.rows)
+        {
+            throw exception("Coincidences Matrix is not square");
+        }
+        const size_t    M = CM.cols;
+        
+        //______________________________________________________________________
+        //
+        // collect all possible coincidences with their ocurences
+        //______________________________________________________________________
+        typedef map<size_t,size_t> dict_t;
+        dict_t dict;
+        
+        for(size_t j=0;j<CM.cols;++j)
+        {
+            const RMatrix<double>::Column &col = CM[j];
+            for(size_t i=0;i<CM.rows;++i)
+            {
+                if(i!=j)
+                {
+                    const size_t key = size_t(col[i]);
+                    size_t *number = dict.search(key);
+                    if(number)
+                    {
+                        assert( (*number)>0 );
+                        ++(*number);
+                    }
+                    else
+                    {
+                        if(!dict.insert(key,1))
+                        {
+                            throw exception("unexpected dict insert failure");
+                        }
+                    }
+                }
+            }
+        }
+        const size_t nu = dict.size();
+        Rprintf("#trials= %u\n", unsigned(M)  );
+        Rprintf("#lambda= %u\n", unsigned(nu) );
+        
+        //______________________________________________________________________
+        //
+        // create the required lambda
+        //______________________________________________________________________
+        vector<size_t> lambda(nu,as_capacity);
+        vector<mpq>    ratio(nu,as_capacity);
+        {
+            const mpn total = M*(M-1);
+            for(dict_t::iterator i=dict.begin();i!=dict.end();++i)
+            {
+                const size_t k = i->key;
+                lambda.push_back(k);
+                const mpz     occurs = *i;
+                const mpq     proba(occurs,total);
+                ratio.push_back(proba);
+            }
+        }
+        
+        
+    }
+    catch(const exception &e )
+    {
+        Rprintf("in wink_xts:\n%s\n%s\n",e.what(),e.when());
+    }
+    catch(...)
+    {
+        Rprintf("unhandled exception in wink_xts\n");
+    }
+    return R_NilValue;
+}
