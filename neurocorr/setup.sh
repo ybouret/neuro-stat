ceb2e091ad553b2482e6770ced6ab4ac68941a36
ybouret <yann.bouret@gmail.com>
2015-09-15 10:01:24 +0200

    minimal make+setup

--- neurocorr/setup.sh
+++ neurocorr/setup.sh
@@ -13,18 +13,14 @@ cmake -E cmake_echo_color --blue --bold "-- Checking yocto sources";
 # is source code here ?
 if [ -d $YOCTO_ROOT ]; then
 	cmake -E cmake_echo_color --black  "-- yocto directory is present";
+	(cd $YOCTO_ROOT && svn up) || xerror "could't update yocto code!";
 else
 	cmake -E cmake_echo_color --black  "-- yocto directory not found..";
 	cmake -E cmake_echo_color --cyan   "-- fetching code by SVN";
 	svn co https://github.com/ybouret/yocto4/trunk $YOCTO_ROOT || xerror "couldn't fetch yocto code!";
 fi
 
-#is sdk ready ?
-YOCTO_SDK="$YOCTO_ROOT/sdk"
-cmake -E cmake_echo_color --blue --bold "-- Checking yocto SDK...";
-if [ -d $YOCTO_SDK ]; then
-	cmake -E cmake_echo_color --black "-- found SDK";
-else
-	cmake -E cmake_echo_color --black "-- need to build SDK...";
-	(cd yocto && $SHELL build-main+r.sh gnu Debug);
-fi
+#(re)building SDK
+cmake -E cmake_echo_color --blue --bold "-- Building yocto SDK...";
+(cd $YOCTO_ROOT && $SHELL build-main+r.sh gnu Debug) || xerror "couldn't build yocto SDK!";
+
