7a3371db10fee14fa18f5bfe1f9fad351f599472
ybouret <yann.bouret@gmail.com>
2015-09-16 20:45:37 +0200

    start step function

--- neurocorr/src/v1/step-function.cpp
+++ neurocorr/src/v1/step-function.cpp
@@ -1,1 +1,58 @@
+#include "step-function.hpp"
 
+
+int CompareCoords(const Coord &lhs, const Coord &rhs) throw()
+{
+    return __compare(lhs.x,rhs.x);
+}
+
+StepFunction:: ~StepFunction() throw()
+{
+}
+
+
+size_t StepFunction::size() const throw()
+{
+    return coords.size();
+}
+
+StepFunction:: StepFunction(size_t n) :
+coords(n,as_capacity)
+{
+
+}
+
+
+void StepFunction:: insert(const double x, const double y)
+{
+    // append the coordinate
+    {
+        const Coord C(x,y);
+        coords.push_back(C);
+    }
+    
+    //check its place
+    for(size_t j=coords.size(),jm=j-1;jm>0;--j,--jm)
+    {
+        Coord &Cm = coords[jm];
+        Coord &C  = coords[j];
+        if(CompareCoords(Cm, C)>0)
+        {
+            bswap(Cm,C);
+        }
+        else
+        {
+            break;
+        }
+    }
+    
+   
+}
+
+size_t StepFunction:: find_index(const double x) const throw()
+{
+    const size_t n = coords.size();
+    assert(n>0);
+    
+    return 0;
+}
--- neurocorr/src/v1/step-function.hpp
+++ neurocorr/src/v1/step-function.hpp
@@ -1,1 +1,35 @@
+#ifndef NC_STEP_FUNCTION_INCLUDED
+#define NC_STEP_FUNCTION_INCLUDED 1
 
+#include "yocto/sequence/vector.hpp"
+#include "yocto/math/v2d.hpp"
+#include "yocto/comparator.hpp"
+
+using namespace yocto;
+
+typedef math::v2d<double> Coord;
+typedef vector<Coord>     CoordVector;
+
+int CompareCoords(const Coord &lhs, const Coord &rhs) throw();
+
+//! Left-Continuous step function
+class StepFunction
+{
+public:
+    virtual ~StepFunction() throw();
+    explicit StepFunction(size_t n);
+    
+    size_t size() const throw();
+    
+    void insert(const double x, const double y);
+    
+    //! find location of x if size()>0
+    size_t find_index(const double x) const throw();
+    
+    
+private:
+    YOCTO_DISABLE_COPY_AND_ASSIGN(StepFunction);
+    CoordVector coords; // assume sorted coordinates
+};
+
+#endif
