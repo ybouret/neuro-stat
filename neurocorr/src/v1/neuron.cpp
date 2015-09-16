aa2cf3443d01a926eeeb5520e5af1187c0813811
ybouret <yann.bouret@gmail.com>
2015-09-16 09:38:02 +0200

    start neuron

--- neurocorr/src/v1/neuron.cpp
+++ neurocorr/src/v1/neuron.cpp
@@ -1,1 +1,10 @@
+#include "neuron.hpp"
 
+
+Neuron:: ~Neuron() throw() {}
+
+Neuron:: Neuron(size_t nTrials) :
+Records(nTrials),
+trials(size)
+{
+}
--- neurocorr/src/v1/neuron.hpp
+++ neurocorr/src/v1/neuron.hpp
@@ -1,1 +1,21 @@
+#ifndef NC_NEURON_INCLUDED
+#define NC_NEURON_INCLUDED 1
+
+#include "record.hpp"
+
+//! Neuron : each Record is a Trial.
+class Neuron : public Records
+{
+public:
+    explicit Neuron(size_t nTrials);
+    virtual ~Neuron() throw();
+
+    const size_t &trials; //!< alias Records::size
+
+private:
+    YOCTO_DISABLE_COPY_AND_ASSIGN(Neuron);
+};
+
+
+#endif
 
