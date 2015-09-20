167b4efc5a6dd4016eb2b17c7fdf0fa0a877f7e6
ybouret <yann.bouret@gmail.com>
2015-09-20 15:16:44 +0200

    ok with test-types

--- neurocorr/src/v2/tests/test-types.cpp
+++ neurocorr/src/v2/tests/test-types.cpp
@@ -1,9 +1,18 @@
 #include "../types.hpp"
 #include "yocto/utest/run.hpp"
+#include "yocto/code/rand.hpp"
 
 YOCTO_UNIT_TEST_IMPL(types)
 {
     
+    Object obj(1000);
+    for(size_t iter=0;iter<10;++iter)
+    {
+        const Real r = alea<double>();
+        const Unit u = obj.toUnit(r);
+        const Real s = obj.toReal(u);
+        std::cerr << r << "->" << u << "->" << s << std::endl;
+    }
 }
 YOCTO_UNIT_TEST_DONE()
 
