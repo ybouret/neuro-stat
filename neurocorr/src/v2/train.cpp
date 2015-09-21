e5c30463af1b474112b86ed641fdba89858ddc96
ybouret <yann.bouret@gmail.com>
2015-09-21 08:01:43 +0200

    init train...

--- neurocorr/src/v2/train.cpp
+++ neurocorr/src/v2/train.cpp
@@ -1,1 +1,2 @@
+#include "train.hpp"
 
--- neurocorr/src/v2/train.hpp
+++ neurocorr/src/v2/train.hpp
@@ -1,1 +1,16 @@
+#ifndef NC_TRAIN_INCLUDED
+#define NC_TRAIN_INCLUDED 1
 
+#include "types.hpp"
+#include "yocto/R/IR.hpp"
+
+class Train : public Object, public LArray<Unit>
+{
+public:
+    virtual ~Train() throw();
+    
+private:
+    YOCTO_DISABLE_COPY_AND_ASSIGN(Train);
+};
+
+#endif
