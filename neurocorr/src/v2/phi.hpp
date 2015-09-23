3d598e3e1f3ef8054194ece704a9013eed04e9b2
ybouret <yann.bouret@gmail.com>
2015-09-23 10:00:20 +0200

    typo, need to compute a CPW value

--- neurocorr/src/v2/phi.hpp
+++ neurocorr/src/v2/phi.hpp
@@ -5,12 +5,13 @@
 
 typedef slots_of<CPW_Function> PhiBase;
 
+//! a set of Constant PieceWise functions for one train of spikes
 class Phi : public PhiBase
 {
 public:
     virtual ~Phi() throw();
 
-    //! allocate memory and compute
+    //! allocate memory and compute 1+extra function
     explicit Phi(const size_t extra,
                  const Real   scale,
                  const Train &train,
