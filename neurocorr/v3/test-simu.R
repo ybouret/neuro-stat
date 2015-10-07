31d57974b8aea58ba4644453d149493bece2f72c
ybouret <yann.bouret@gmail.com>
2015-10-07 11:32:51 +0200

    ok for test simus, need to build big data

--- neurocorr/v3/test-simu.R
+++ neurocorr/v3/test-simu.R
@@ -9,10 +9,28 @@ scale=1e6;
 delta=0.005;
 K=20;
 
+stamp = proc.time();
 seq_mtx=NeuroCorr_Compute(DataNeur,Nneur,scale,delta,K,BoxEst,"byKind");
+seq_tmx = proc.time() - stamp;
 
 NeuroCorr_SetParallel(4);
 
+stamp = proc.time();
 par_mtx=NeuroCorr_Compute(DataNeur,Nneur,scale,delta,K,BoxEst,"byKind");
+par_tmx = proc.time() - stamp;
 
+## check the same
+differences=c(
+norm(seq_mtx$mu1$'1' - par_mtx$mu1$'1',type="f"),
+norm(seq_mtx$mu2$'1' - par_mtx$mu2$'1',type="f"),
+norm(seq_mtx$muA$'1' - par_mtx$muA$'1',type="f"),
+norm(seq_mtx$G$'1'   - par_mtx$G$'1',  type="f")
+)
+## must be 0...
+print(differences);
+
+speedUp = seq_tmx[[3]]/par_tmx[[3]];
+print(paste("speedUp=",speedUp));
+
+## todo before leaving R...
 NeuroCorr_CleanUp();
