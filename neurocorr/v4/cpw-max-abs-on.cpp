e9eed3ac6b8e350c0c13cfd915ed399714baa81e
ybouret <yann.bouret@gmail.com>
2015-10-21 12:02:59 +0200

    ok for MaxOn...

--- neurocorr/v4/cpw-max-abs-on.cpp
+++ neurocorr/v4/cpw-max-abs-on.cpp
@@ -26,6 +26,10 @@ Unit CPW:: maxAbsOn(const Unit tauStart, const Unit tauFinal) const throw()
     }
     else
     {
+        //______________________________________________________________________
+        //
+        // Special Cases
+        //______________________________________________________________________
         const size_t n = size();
         switch(n)
         {
@@ -56,11 +60,16 @@ Unit CPW:: maxAbsOn(const Unit tauStart, const Unit tauFinal) const throw()
                 break;
         }
 
+        //______________________________________________________________________
+        //
+        // Generic Cases
+        //______________________________________________________________________
         assert(n>=2);
         const coord lower = front();
         if(tauFinal<=lower.tau)
         {
             // all at left
+            //std::cerr << "|all left|" << std::endl;
             return UnitAbs(foot);
         }
         else
@@ -68,16 +77,67 @@ Unit CPW:: maxAbsOn(const Unit tauStart, const Unit tauFinal) const throw()
             const coord upper = back();
             if(tauStart>=upper.tau)
             {
+                // all at right
+                //std::cerr << "|all right|" << std::endl;
                 return UnitAbs(upper.value);
             }
             else
             {
+                assert(tauFinal>lower.tau);
+                assert(tauStart<upper.tau);
                 //______________________________________________________________
                 //
                 // generic case
                 //______________________________________________________________
+                const CPW &self = *this;
+                Unit       ans  = 0;
 
-                return 0;
+                // Initialize
+                size_t i = 1;
+                if(tauStart<lower.tau)
+                {
+                    ans = UnitAbs(foot);
+                }
+                else
+                {
+                    // find values such that tau[jlo]<=tauStart<tau[jhi]
+                    size_t  jlo = 1;
+                    size_t &jhi = i;
+                    jhi         = n;
+                    while(jhi-jlo>1)
+                    {
+                        const size_t jmid = (jlo+jhi)>>1;
+                        const Unit   tmid = self[jmid].tau;
+                        if(tauStart<tmid)
+                        {
+                            jhi = jmid;
+                        }
+                        else
+                        {
+                            jlo = jmid;
+                        }
+                    }
+                    assert(1==jhi-jlo);
+                    assert(self[jlo].tau<=tauStart);
+                    assert(tauStart<self[jhi].tau);
+                    ans = UnitAbs(self[jlo].value);
+                }
+
+                // starting from i until > tauFinal (or not)
+                for(;i<=n;++i)
+                {
+                    const coord curr = self[i];
+                    if(curr.tau>=tauFinal)
+                        break;
+                    const Unit tmp = UnitAbs(curr.value);
+                    if(tmp>ans)
+                    {
+                        ans=tmp;
+                    }
+                }
+
+
+                return ans;
             }
         }
         
--- neurocorr/v4/tests/test-abs.cpp
+++ neurocorr/v4/tests/test-abs.cpp
@@ -5,7 +5,7 @@
 static inline
 void do_test( const CPW &F , uint64_t &raw64, uint64_t &opt64)
 {
-    std::cerr << "F.size=" << F.size() << std::endl;
+    std::cerr << std::endl << "F.size=" << F.size() << std::endl;
     
     uint64_t mark  = 0;
 
@@ -20,7 +20,7 @@ void do_test( const CPW &F , uint64_t &raw64, uint64_t &opt64)
     {
         for(Unit w=1;w<=wmax;++w)
         {
-            std::cerr << ".";
+            //std::cerr << ".";
             const Unit tauEnd = tau+w;
             mark = chrono.ticks();
             const Unit raw    = F.maxAbsOn_(tau,tauEnd);
@@ -32,17 +32,24 @@ void do_test( const CPW &F , uint64_t &raw64, uint64_t &opt64)
 
             if(raw!=opt)
             {
+                std::cerr << "Bad!" << std::endl;
+                std::cerr << "raw=" << raw << std::endl;
+                std::cerr << "opt=" << opt << std::endl;
+                std::cerr << "tauStart=" << tau << std::endl;
+                std::cerr << "tauFinal=" << tauEnd << std::endl;
+                std::cerr << "foot=" << F.foot << std::endl;
+                std::cerr << "points=" << F << std::endl;
                 throw exception("Mismatch :raw=%ld, opt=%ld!", long(raw), long(opt) );
             }
         }
     }
-    std::cerr << std::endl;
     const double rawTime = chrono(raw64);
     const double optTime = chrono(opt64);
     std::cerr << "rawTime=" << rawTime << std::endl;
     std::cerr << "optTime=" << optTime << std::endl;
 }
 
+#include "yocto/code/rand.hpp"
 
 YOCTO_UNIT_TEST_IMPL(abs)
 {
@@ -60,7 +67,26 @@ YOCTO_UNIT_TEST_IMPL(abs)
     raw64 = opt64 = 0;
     F.add(1,3);
     do_test(F,raw64,opt64);
-    
+
+    for(size_t np=2;np<=50;++np)
+    {
+        raw64 = opt64 = 0;
+        for(size_t iter=0;iter<1;++iter)
+        {
+            F.free();
+            Unit curr = 0;
+            for(size_t i=1;i<=np;++i)
+            {
+                curr += 1 + Unit(alea_leq(5));
+                const Unit val = -10 + Unit(alea_leq(20));
+                F.add(curr,val);
+            }
+            do_test(F,raw64,opt64);
+        }
+        const double speedUp = double(raw64)/double(opt64);
+        std::cerr << "\tSpeedUp" << np << " : " << speedUp << std::endl;
+    }
+
 
 }
 YOCTO_UNIT_TEST_DONE()
