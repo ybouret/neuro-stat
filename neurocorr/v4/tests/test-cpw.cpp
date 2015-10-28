3be5c152a3f45033172038cd6ac0401e06a67f79
ybouret <yann.bouret@gmail.com>
2015-10-28 09:30:49 +0100

    need to test algorithm, recompute gen samples...

--- neurocorr/v4/tests/test-cpw.cpp
+++ neurocorr/v4/tests/test-cpw.cpp
@@ -1,6 +1,20 @@
 #include "../cpw.hpp"
 #include "yocto/utest/run.hpp"
 
+
+static inline void check_intg(const CPW &F)
+{
+    if(F.size()>0)
+    {
+        const Unit a = F.front().tau - 1;
+        const Unit b = F.back().tau  + 1;
+        if( F.integrate(a,b) != F.integrate_(a,b) )
+        {
+            throw exception("check intg mismatch");
+        }
+    }
+}
+
 YOCTO_UNIT_TEST_IMPL(cpw)
 {
     CPW F;
@@ -12,36 +26,37 @@ YOCTO_UNIT_TEST_IMPL(cpw)
     std::cerr << "f1=" << F << std::endl;
     F.save("f1.dat");
     F.save_sample("f1v.dat");
+    check_intg(F);
 
     F.add(-1,2);
     std::cerr << "f2=" << F << std::endl;
     F.save("f2.dat");
     F.save_sample("f2v.dat");
-
+    check_intg(F);
 
     Train train;
     vector<Unit> shift;
     F.buildFrom(train, 5,shift);
     F.save("phi0.dat");
     F.save_sample("phi0v.dat");
-
+    check_intg(F);
 
     train.push_back(0);
     F.buildFrom(train, 5, shift);
     F.save("phi1.dat");
     F.save_sample("phi1v.dat");
-
+    check_intg(F);
 
     train.push_back(1);
     F.buildFrom(train,5,shift);
     F.save("phi2.dat");
     F.save_sample("phi2v.dat");
-
+    check_intg(F);
 
     train.push_back(5);
     F.buildFrom(train,5,shift);
     F.save("phi3.dat");
     F.save_sample("phi3v.dat");
-
+    check_intg(F);
 }
 YOCTO_UNIT_TEST_DONE()
