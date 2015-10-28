8508ccaf76c4b251056c1baf64405e3bf405e2d5
ybouret <yann.bouret@gmail.com>
2015-10-28 09:23:40 +0100

    seems ok for intg

--- neurocorr/v4/tests/test-intg.cpp
+++ neurocorr/v4/tests/test-intg.cpp
@@ -38,12 +38,71 @@ void do_intg( const CPW &F )
         }
         fp("\n");
     }
-    
-    
 }
+
+#include "yocto/sys/wtime.hpp"
+
+static inline
+void perf_intg(const CPW &F, uint64_t &raw64, uint64_t &opt64, wtime &chrono )
+{
+    uint64_t mark = 0;
+
+
+    Unit tauMin = 0;
+    Unit tauMax = 0;
+    if(F.size()<=0)
+    {
+        tauMin = -10;
+        tauMax =  10;
+    }
+    else
+    {
+        tauMin = F.front().tau-10;
+        tauMax = F.back() .tau+10;
+    }
+    const Unit W = (tauMax-tauMin);
+    for(Unit tauStart=tauMin;tauStart<=tauMax;++tauStart)
+    {
+        for(Unit w=1;w<=W;++w)
+        {
+            const Unit tauFinal = tauStart+w;
+            if(tauFinal>tauMax)
+                break;
+
+            mark = chrono.ticks();
+            const Unit raw = F.integrate_(tauStart,tauFinal);
+            raw64 += chrono.ticks()-mark;
+
+            mark = chrono.ticks();
+            const Unit opt = F.integrate(tauStart,tauFinal);
+            opt64 += chrono.ticks()-mark;
+
+            if(raw!=opt)
+            {
+                throw exception("intg mismatch");
+            }
+        }
+    }
+
+}
+
+static inline double display( const uint64_t r, const uint64_t o, wtime &chrono)
+{
+    const double rTime = chrono(r);
+    const double oTime = chrono(o);
+    std::cerr << "raw_time=" << rTime << std::endl;
+    std::cerr << "opt_time=" << oTime << std::endl;
+    const double ans = rTime/oTime;
+    std::cerr << "speed_up=" << ans << std::endl;
+    return ans;
+}
+
+#include "yocto/code/rand.hpp"
 
 YOCTO_UNIT_TEST_IMPL(intg)
 {
+    wtime   chrono;
+    chrono.start();
 
     CPW F;
 
@@ -61,6 +120,37 @@ YOCTO_UNIT_TEST_IMPL(intg)
 
     F.add(8,5);
     do_intg(F);
+
+    std::cerr << "perf/check..." << std::endl;
+    uint64_t raw64 = 0;
+    uint64_t opt64 = 0;
+
+    ios::ocstream::overwrite("intgperf.dat");
+    F.free();
+    F.foot = 1;
+    for(size_t n=0;n<=30;++n)
+    {
+        const size_t num_iter = 10 + 1000/((n+1)*(n+1));
+        std::cerr << "#iter=" << num_iter << std::endl;
+        for(size_t iter=0;iter<num_iter;++iter)
+        {
+            F.free();
+            Unit curr = 0;
+            for(size_t i=0;i<n;++i)
+            {
+                curr += 1+alea_leq(10);
+                F.add(curr,i+2);
+            }
+            perf_intg(F,raw64,opt64,chrono);
+        }
+        std::cerr << "-- n=" << n << std::endl;
+        const double ans = display(raw64,opt64,chrono);
+        ios::acstream fp("intgperf.dat");
+        fp("%u %g\n", unsigned(n), ans);
+        raw64 = 0;
+        opt64 = 0;
+    }
+
 
 }
 YOCTO_UNIT_TEST_DONE()
